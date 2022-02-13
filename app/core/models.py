d0aba13f9d5209641b0974cfe51da4c46b448eee
Soroush Mehraban <Smehraban2013@gmail.com>
2022-02-13 17:03:57 +0330

    Models are created. Home page front-end side is also created

---
+++ app/core/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Movie(models.Model):
+    """Movie model for showing a movie in a movie list"""
+    name = models.CharField(max_length=100)
+    poster = models.ImageField(upload_to='posters/')
+    director_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    """Comment from a user on a movie"""
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"From {self.user.username} on {self.movie}"
